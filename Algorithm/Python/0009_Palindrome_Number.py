cca7c515a08b56bb364a0c8098535d482fdcfcd1
fantasylsc <32818137+fantasylsc@users.noreply.github.com>
2019-10-22 10:08:02 -0600

    Palindrome Number Easy

---
+++ Algorithm/Python/0009_Palindrome_Number.py
@@ -0,0 +1,39 @@
+'''
+
+Determine whether an integer is a palindrome. An integer is a palindrome when it reads the same backward as forward.
+
+Example 1:
+
+Input: 121
+Output: true
+Example 2:
+
+Input: -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+Example 3:
+
+Input: 10
+Output: false
+Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+
+'''
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        
+        original = x
+        reverse = 0
+        
+        while x > 0:
+            digit = x % 10
+            x = x // 10
+            reverse = reverse * 10 + digit
+        
+        return reverse == original
+
+
+
+
