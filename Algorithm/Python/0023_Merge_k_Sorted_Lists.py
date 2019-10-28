c17da91cf5de9d04ac1bcf305b6e8bc8fef8859b
fantasylsc <32818137+fantasylsc@users.noreply.github.com>
2019-10-28 14:43:37 -0600

    0023_Merge_k_Sorted_Lists.py

---
+++ Algorithm/Python/0023_Merge_k_Sorted_Lists.py
@@ -0,0 +1,57 @@
+'''
+
+Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
+
+Example:
+
+Input:
+[
+  1->4->5,
+  1->3->4,
+  2->6
+]
+Output: 1->1->2->3->4->4->5->6
+
+'''
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
+        def mergeTwo(l1, l2):
+            dummy = ListNode(0)
+            head = dummy
+            
+            while l1 and l2:
+                if l1.val <= l2.val:
+                    head.next = l1
+                    l1 = l1.next
+                else:
+                    head.next = l2
+                    l2 = l2.next
+                head = head.next
+                
+            if not l1:
+                head.next = l2
+            if not l2:
+                head.next = l1
+                
+            return dummy.next
+
+        
+        amount = len(lists)
+        interval = 1
+        while interval < amount:
+            for i in range(0, amount - interval, interval * 2):
+                lists[i] = mergeTwo(lists[i], lists[i + interval])
+            interval *= 2
+        return lists[0] if amount > 0 else None
+        
+
+
+
