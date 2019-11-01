3bc77a74d585ff1f40af4c91809e140f370f7976
fantasylsc <32818137+fantasylsc@users.noreply.github.com>
2019-11-01 16:55:15 -0600

    0054_Spiral_Matrix.py

--- Algorithm/Python/75/0054_Spiral_Matrix.py
+++ Algorithm/Python/75/0054_Spiral_Matrix.py
@@ -25,24 +25,42 @@ Output: [1,2,3,4,8,12,11,10,9,5,6,7]
 
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        if not matrix: return []
-        R, C = len(matrix), len(matrix[0])
-        seen = [[False] * C for _ in matrix]
-        ans = []
-        dr = [0, 1, 0, -1]
-        dc = [1, 0, -1, 0]
-        r = c = di = 0
-        for _ in range(R * C):
-            ans.append(matrix[r][c])
-            seen[r][c] = True
-            cr, cc = r + dr[di], c + dc[di]
-            if 0 <= cr < R and 0 <= cc < C and not seen[cr][cc]:
-                r, c = cr, cc
-            else:
-                di = (di + 1) % 4
-                r, c = r + dr[di], c + dc[di]
-        return ans      
-
+        if len(matrix) == 0 or len(matrix[0]) == 0:
+            return []
+        
+        m = len(matrix)
+        n = len(matrix[0])
+        res = []
+        up = 0
+        down = m - 1
+        left = 0
+        right = n - 1
+        while True:
+            for j in range(left, right + 1):
+                res.append(matrix[up][j])
+            up += 1
+            if up > down:
+                break
+                
+            for i in range(up, down + 1):
+                res.append(matrix[i][right])
+            right -= 1
+            if right < left:
+                break
+                
+            for j in range(right, left - 1, -1):
+                res.append(matrix[down][j])
+            down -= 1
+            if down < up:
+                break
+                
+            for i in range(down, up - 1, -1):
+                res.append(matrix[i][left])
+            left += 1
+            if left > right:
+                break
+                
+        return res
 
         
 
