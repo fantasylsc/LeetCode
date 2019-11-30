6515505c511fd476172912737aca354ff18cc3b2
fantasylsc <32818137+fantasylsc@users.noreply.github.com>
2019-11-30 15:41:44 -0700

    0322_Coin_Change.py

--- Algorithm/Python/325/0322_Coin_Change.py
+++ Algorithm/Python/325/0322_Coin_Change.py
@@ -16,6 +16,63 @@ You may assume that you have an infinite number of each kind of coin.
 
 '''
 
+# DP
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        # initialize dp table
+        max_value = amount + 1
+        dp = [max_value] * max_value
+        dp[0] = 0
+        
+        # for every amount, try every coin to update dp table
+        for i in range(1, amount + 1):
+            for coin in coins:
+                if coin <= i:
+                    dp[i] = min(dp[i], dp[i - coin] + 1)
+        if dp[amount] > amount:
+            return -1
+        else:
+            return dp[amount]
+
+
+# Recusion without memorization
+
+# class Solution:
+#     def coinChange(self, coins: List[int], amount: int) -> int:
+#         # Recursion with memorization
+#         if amount < 1:
+#             return 0
+#         # self.count = [0] * amount
+#         return self.helper(coins, amount)
+        
+#     def helper(self, coins, rem):
+#         # Base case
+#         if rem < 0:
+#             return -1
+#         if rem == 0:
+#             return 0
+        
+#         # Memorization
+#         # if self.count[rem - 1] != 0:
+#         #     return self.count[rem - 1]
+        
+#         # for each coin in coins, update optimal minimum value
+#         min_value = float('inf')
+#         for coin in coins:
+#             temp = self.helper(coins, rem - coin)
+#             if temp >= 0 and temp < min_value:
+#                 min_value = temp + 1
+                
+#         if min_value == float('inf'):
+#             return -1
+#         else:
+#             return min_value
+#         # if min_value == float('inf'): # no update means cannot be made up by any combination of the coins
+#         #     self.count[rem - 1] = -1
+#         # else:
+#         #     self.count[rem - 1] = min_value # update with min_value
+#         # return self.count[rem - 1]
+
 # Recursion with memorization
 # class Solution:
 #     def coinChange(self, coins: List[int], amount: int) -> int:
@@ -49,22 +106,6 @@ You may assume that you have an infinite number of each kind of coin.
 #         return self.count[rem - 1]
 
 
-# DP
-class Solution:
-    def coinChange(self, coins: List[int], amount: int) -> int:
-        # initialize dp table
-        max_value = amount + 1
-        dp = [max_value] * max_value
-        dp[0] = 0
-        
-        # for every amount, try every coin to update dp table
-        for i in range(1, amount + 1):
-            for coin in coins:
-                if coin <= i:
-                    dp[i] = min(dp[i], dp[i - coin] + 1)
-        if dp[amount] > amount:
-            return -1
-        else:
-            return dp[amount]
+
 
 
