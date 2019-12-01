b9ea9c5dfe382e94008e38c340277ebe3f6def51
fantasylsc <32818137+fantasylsc@users.noreply.github.com>
2019-11-30 20:47:03 -0700

    0518_Coin_Change_2.py

--- Algorithm/Python/525/0518_Coin_Change_2.py
+++ Algorithm/Python/525/0518_Coin_Change_2.py
@@ -35,19 +35,30 @@ the answer is guaranteed to fit into signed 32-bit integer
 
 '''
 
+# class Solution:
+#     def change(self, amount: int, coins: List[int]) -> int:
+#         dp = [[0] * (amount + 1) for _ in range(len(coins) + 1)]
+#         dp[0][0] = 1
+#         for i in range(1, len(coins) + 1):
+#             dp[i][0] = 1 
+#             for j in range(1, amount + 1):
+#                 if j >= coins[i - 1]:
+#                     dp[i][j] = dp[i - 1][j] + dp[i][j - coins[i - 1]] # without ith coin + with ith coin
+#                 else:
+#                     dp[i][j] = dp[i - 1][j]
+#         return dp[len(coins)][amount]
+    
+    
+# Optimize space
+# dp[i][j] = dp[i - 1][j] + dp[i][j - coins[i - 1]] -> dp[i] += dp[i - coin]
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        dp = [[0] * (amount + 1) for _ in range(len(coins) + 1)]
-        dp[0][0] = 1
-        for i in range(1, len(coins) + 1):
-            dp[i][0] = 1 
-            for j in range(1, amount + 1):
-                if j >= coins[i - 1]:
-                    dp[i][j] = dp[i - 1][j] + dp[i][j - coins[i - 1]] # coins[i - 1] is the ith coin in coins, dp added 0 dimension 
-                else:
-                    dp[i][j] = dp[i - 1][j]
-        return dp[len(coins)][amount]
-
+        dp = [0] * (amount + 1)
+        dp[0] = 1
+        for coin in coins:
+            for i in range(coin, amount + 1):
+                dp[i] += dp[i - coin]
+        return dp[amount]
 
 
 
