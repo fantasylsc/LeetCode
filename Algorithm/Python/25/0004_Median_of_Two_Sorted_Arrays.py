4fbaad4d771870a8640555a0aa831e9b0066791a
fantasylsc <32818137+fantasylsc@users.noreply.github.com>
2019-12-05 12:07:20 -0700

    0004_Median_of_Two_Sorted_Arrays.py

--- Algorithm/Python/25/0004_Median_of_Two_Sorted_Arrays.py
+++ Algorithm/Python/25/0004_Median_of_Two_Sorted_Arrays.py
@@ -15,18 +15,20 @@ The median is 2.0
 
 '''
 
-import sys
-
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         m = len(nums1)
         n = len(nums2)
+        # for a sorted list with k numbers, the median is the average of the (k + 1)//2 th number and (k + 2)//2 the number.
         left = (m + n + 1) // 2
         right = (m + n + 2) // 2
         
         return (self.findKth(nums1, 0, nums2, 0, left) + self.findKth(nums1, 0, nums2, 0, right)) / 2
     
+    # find kth number in nums1 starts from ith item and nums2 starts from jth item
     def findKth(self, nums1, i, nums2, j, k):
+        
+        # i exceed lenth of nums1, nums1 = [], so find kth number in nums2
         if i >= len(nums1):
             return nums2[j + k - 1]
         if j >= len(nums2):
@@ -34,15 +36,19 @@ class Solution:
         if k == 1:
             return min(nums1[i], nums2[j])
         
-        if i + k/2 - 1 < len(nums1):
+        
+        # nums1 = {1, 3}，nums2 = {2, 4, 5}，K=4, eliminate {1, 3} in nums1
+
+        if i + k/2 - 1 < len(nums1): # if k/2th item exists, choose this item
             midVal1 = nums1[i + k//2 - 1]
-        else:
-            midVal1 = sys.maxsize
+        else: # Example: nums1 = {3}，nums2 = {2, 4, 5, 6, 7}，K=4. Should keep nums1 (set midVla1 = float('inf')), eliminate {2, 4} in nums2
+            midVal1 = float('inf')
         if j + k/2 - 1 < len(nums2):
             midVal2 = nums2[j + k//2 - 1]
         else:
-            midVal2 = sys.maxsize
-            
+            midVal2 = float('inf')
+        
+        # Eliminate the k//2 values
         if midVal1 < midVal2:
             return self.findKth(nums1, i + k//2, nums2, j, k - k//2)
         else:
