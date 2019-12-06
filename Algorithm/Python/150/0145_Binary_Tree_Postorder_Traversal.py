fd8ff3f3319d809f0016bbfcbed74f9cd0a4a431
fantasylsc <32818137+fantasylsc@users.noreply.github.com>
2019-12-06 16:28:18 -0700

    0145_Binary_Tree_Postorder_Traversal.py

---
+++ Algorithm/Python/150/0145_Binary_Tree_Postorder_Traversal.py
@@ -0,0 +1,104 @@
+
+'''
+
+Given a binary tree, return the postorder traversal of its nodes' values.
+
+Example:
+
+Input: [1,null,2,3]
+   1
+    \
+     2
+    /
+   3
+
+Output: [3,2,1]
+Follow up: Recursive solution is trivial, could you do it iteratively?
+
+'''
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+# Iterative solution:
+# postorder sequence: left, right, root
+# using root, right, left traversal, then reverse the result
+
+# Iterative solution 1
+# postorder (left, right, root),  use traversal order (root, right, left) and append node reversely
+
+from collections import deque
+
+class Solution:
+    def postorderTraversal(self, root: TreeNode) -> List[int]:
+        if not root:
+            return []
+        res = deque()
+        stack = [root]
+        
+        while stack:
+            current = stack.pop()
+            res.appendleft(current.val)
+            if current.left:
+                stack.append(current.left)
+            if current.right:
+                stack.append(current.right)
+        return res
+
+        
+        
+# Iterative solution 2
+# Idea: (left, right, root) When we process a node, this node should be left node or its child node was visited
+
+# class Solution:
+#     def postorderTraversal(self, root: TreeNode) -> List[int]:
+#         if not root:
+#             return []
+        
+#         res = []
+#         stack = [root]
+#         head = root # record last visited node
+        
+#         while stack:
+#             t = stack[-1] # check whether need to process t or not
+#             # if t is a leaf node or t.left or t.right was processed, then process node t
+#             if (not t.left and not t.right) or t.left == head or t.right == head:
+#                 res.append(t.val)
+#                 stack.pop()
+#                 head = t
+#             else:
+#                 # make sure process left first
+#                 if t.right:
+#                     stack.append(t.right)
+#                 if t.left:
+#                     stack.append(t.left)
+#         return res
+        
+        
+# Recursive solution:
+
+# class Solution:
+#     def postorderTraversal(self, root: TreeNode) -> List[int]:
+#         self.res = []
+#         self.helper(root)
+#         return self.res
+        
+#     def helper(self, root):
+#         if not root:
+#             return
+        
+#         self.helper(root.left)
+#         self.helper(root.right)
+#         self.res.append(root.val)
+        
+        
+            
+        
+
+
+
+
