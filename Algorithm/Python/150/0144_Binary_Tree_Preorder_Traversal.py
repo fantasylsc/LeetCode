b2e4e70edc84123cea221e25ff42671a935fea67
fantasylsc <32818137+fantasylsc@users.noreply.github.com>
2019-12-06 14:19:21 -0700

    0144_Binary_Tree_Preorder_Traversal.py

---
+++ Algorithm/Python/150/0144_Binary_Tree_Preorder_Traversal.py
@@ -0,0 +1,68 @@
+'''
+
+Given a binary tree, return the preorder traversal of its nodes' values.
+
+Example:
+
+Input: [1,null,2,3]
+   1
+    \
+     2
+    /
+   3
+
+Output: [1,2,3]
+Follow up: Recursive solution is trivial, could you do it iteratively?
+
+'''
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution:
+    def preorderTraversal(self, root: TreeNode) -> List[int]:
+        if root == None:
+            return []
+        res = []
+        stack = []
+        stack.append(root)
+        
+        while stack:
+            temp = stack.pop()
+            res.append(temp.val)
+            
+            # for preorder traversal, append right first, then left
+            if temp.right:
+                stack.append(temp.right)
+            if temp.left:
+                stack.append(temp.left)
+        return res
+                
+        
+# Recursive solution:
+
+# class Solution:
+#     def preorderTraversal(self, root: TreeNode) -> List[int]:
+#         self.res = []
+#         self.helper(root)
+#         return self.res
+    
+#     def helper(self, root):
+#         if root == None:
+#             return
+#         self.res.append(root.val)
+#         self.helper(root.left)
+#         self.helper(root.right)
+        
+        
+            
+        
+
+
+
+
