66363e21cdba118f573ad2198756154f36907d5c
fantasylsc <32818137+fantasylsc@users.noreply.github.com>
2019-12-05 17:20:06 -0700

    0081_Search_in_Rotated_Sorted_Array_II.py

---
+++ Algorithm/Python/100/0081_Search_in_Rotated_Sorted_Array_II.py
@@ -0,0 +1,69 @@
+'''
+
+Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+
+(i.e., [0,0,1,2,2,5,6] might become [2,5,6,0,0,1,2]).
+
+You are given a target value to search. If found in the array return true, otherwise return false.
+
+Example 1:
+
+Input: nums = [2,5,6,0,0,1,2], target = 0
+Output: true
+Example 2:
+
+Input: nums = [2,5,6,0,0,1,2], target = 3
+Output: false
+Follow up:
+
+This is a follow up problem to Search in Rotated Sorted Array, where nums may contain duplicates.
+Would this affect the run-time complexity? How and why?
+
+'''
+
+# consider conditions like:
+# nums[start] == nums[mid] and nums[end] == nums[mid], end -= 1
+# nums[start] == nums[mid], discard [start, mid]
+# nums[end] == nums[mid], discard [mid, end]
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+        if not nums:
+            return False
+        
+        start = 0
+        end = len(nums) - 1
+        
+        while start + 1 < end:
+            mid = start + (end - start) // 2
+            if nums[mid] == target:
+                return True
+            if nums[start] == nums[mid] and nums[end] == nums[mid]:
+                end -= 1
+            elif nums[start] == nums[mid]:
+                start = mid
+            elif nums[end] == nums[mid]:
+                end = mid
+            elif nums[mid] > nums[start]:
+                if nums[start] <= target and target <= nums[mid]:
+                    end = mid
+                else:
+                    start = mid
+            else:
+                if nums[mid] <= target and target <= nums[end]:
+                    start = mid
+                else:
+                    end = mid
+        print(start, end)
+        if nums[start] == target:
+            return True
+        if nums[end] == target:
+            return True
+        return False
+        
+        
+        
+        
+
+
+
