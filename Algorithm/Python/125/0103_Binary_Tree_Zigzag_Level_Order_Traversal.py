61f418a872176e45f581f32d7de8f5c09365fc10
fantasylsc <32818137+fantasylsc@users.noreply.github.com>
2019-12-09 14:54:56 -0700

    0103_Binary_Tree_Zigzag_Level_Order_Traversal.py

---
+++ Algorithm/Python/125/0103_Binary_Tree_Zigzag_Level_Order_Traversal.py
@@ -0,0 +1,61 @@
+'''
+
+Given a binary tree, return the zigzag level order traversal of its nodes' values. (ie, from left to right, then right to left for the next level and alternate between).
+
+For example:
+Given binary tree [3,9,20,null,null,15,7],
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its zigzag level order traversal as:
+[
+  [3],
+  [20,9],
+  [15,7]
+]
+
+'''
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+from collections import deque
+
+class Solution:
+    def zigzagLevelOrder(self, root: TreeNode) -> List[List[int]]:
+        if not root:
+            return []
+        
+        res = []
+        queue = deque()
+        queue.append(root)
+        level = 0
+        
+        while queue:
+            level += 1
+            temp = []
+            for _ in range(len(queue)):
+                curr = queue.popleft()
+                temp.append(curr.val)
+                if curr.left:
+                    queue.append(curr.left)
+                if curr.right:
+                    queue.append(curr.right)
+            if level % 2 == 1:
+                res.append(temp)
+            else:
+                res.append(temp[::-1])
+        return res
+        
+        
+    
+    
+
+
+
