6d601d160b1679ffecd85b5a8a728e18bc9bcecd
fantasylsc <32818137+fantasylsc@users.noreply.github.com>
2019-12-23 10:42:21 -0700

    0077_Combinations.py

---
+++ Algorithm/Python/100/0077_Combinations.py
@@ -0,0 +1,35 @@
+'''
+
+Given two integers n and k, return all possible combinations of k numbers out of 1 ... n.
+
+Example:
+
+Input: n = 4, k = 2
+Output:
+[
+  [2,4],
+  [3,4],
+  [2,3],
+  [1,2],
+  [1,3],
+  [1,4],
+]
+
+'''
+
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        self.res = []
+        curr = []
+        self.helper(n, k, 1, curr)
+        return self.res
+        
+    def helper(self, n, k, start_level, curr):
+        if len(curr) == k:
+            self.res.append(curr[:])
+            return
+        for i in range(start_level, n + 1):
+            curr.append(i)
+            self.helper(n, k, i + 1, curr) # i + 1 (without duplicate) or start_level (with duplicate)
+            curr.pop()
+        
