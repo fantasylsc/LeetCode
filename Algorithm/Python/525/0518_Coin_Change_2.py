99fe09ce1b753690e7412f221372e5fe402e36bb
fantasylsc <32818137+fantasylsc@users.noreply.github.com>
2019-12-23 21:25:40 -0700

    Update 0518_Coin_Change_2.py

--- Algorithm/Python/525/0518_Coin_Change_2.py
+++ Algorithm/Python/525/0518_Coin_Change_2.py
@@ -55,8 +55,8 @@ class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
         dp = [0] * (amount + 1)
         dp[0] = 1
-        for coin in coins:
-            for i in range(coin, amount + 1):
+        for coin in coins: # Add coin one by one
+            for i in range(coin, amount + 1): # amount less than current coin is not affected
                 dp[i] += dp[i - coin]
         return dp[amount]
 
