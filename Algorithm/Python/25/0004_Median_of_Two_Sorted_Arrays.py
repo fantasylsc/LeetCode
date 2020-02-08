4faeb767b25c35be5d36ed6a1d5f1b8ffb406fd8
fantasylsc <32818137+fantasylsc@users.noreply.github.com>
2020-02-07 20:28:12 -0700

    Update 0004_Median_of_Two_Sorted_Arrays.py

--- Algorithm/Python/25/0004_Median_of_Two_Sorted_Arrays.py
+++ Algorithm/Python/25/0004_Median_of_Two_Sorted_Arrays.py
@@ -14,27 +14,21 @@ nums2 = [2]
 The median is 2.0
 
 '''
-# for a sorted list with k numbers, the median is the average of the (k + 1)//2 th number and (k + 2)//2 the number. 
-# So the question is converted to find the (k + 1)//2 th number and (k + 2)//2 the number.
-# Find kth: findKth(self, nums1, i, nums2, j, k)
-# Base case for recursion: i, len(nums1), j, len(nums2), k == 1
-# See if k/2th item exists, midVal1, midVal2, if not exist, set midVal = float('int')
-# Eliminate the smaller k/2th number, update recursion
+# find (m + n + 1)//2 and (m + n + 2)//2
+# findkth(nums1, i, nums2, j, k)
+# 1) if i >= len(nums1) 2) if i + k//2 - 1 < len(nums1) 3) if midVal1 < midVal2
+
 
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         m = len(nums1)
         n = len(nums2)
+        left = (m + n + 1)//2
+        right = (m + n + 2)//2
         
-        left = (m + n + 1) // 2
-        right = (m + n + 2) // 2
+        return (self.findkth(nums1, 0, nums2, 0, left) + self.findkth(nums1, 0, nums2, 0, right)) / 2
         
-        return (self.findKth(nums1, 0, nums2, 0, left) + self.findKth(nums1, 0, nums2, 0, right)) / 2
-    
-    # find kth number in nums1 starts from ith item and nums2 starts from jth item
-    def findKth(self, nums1, i, nums2, j, k):
-        
-        # i exceed lenth of nums1, nums1 = [], so find kth number in nums2
+    def findkth(self, nums1, i, nums2, j, k):
         if i >= len(nums1):
             return nums2[j + k - 1]
         if j >= len(nums2):
@@ -42,23 +36,18 @@ class Solution:
         if k == 1:
             return min(nums1[i], nums2[j])
         
-        
-        # nums1 = {1, 3}，nums2 = {2, 4, 5}，K=4, eliminate {1, 3} in nums1
-
-        if i + k/2 - 1 < len(nums1): # if k/2th item exists, choose this item
+        if i + k//2 - 1 < len(nums1):
             midVal1 = nums1[i + k//2 - 1]
-        else: # Example: nums1 = {3}，nums2 = {2, 4, 5, 6, 7}，K=4. Should keep nums1 (set midVla1 = float('inf')), eliminate {2, 4} in nums2
-            midVal1 = float('inf')
-        if j + k/2 - 1 < len(nums2):
+        else:
+            midVal1 =  float('inf')
+        if j + k//2 - 1 < len(nums2):
             midVal2 = nums2[j + k//2 - 1]
         else:
             midVal2 = float('inf')
-        
-        # Eliminate the k//2 values
+            
         if midVal1 < midVal2:
-            return self.findKth(nums1, i + k//2, nums2, j, k - k//2)
+            return self.findkth(nums1, i + k//2, nums2, j, k - k//2)
         else:
-            return self.findKth(nums1, i , nums2, j + k//2, k - k//2)
-
-
-
+            return self.findkth(nums1, i, nums2, j + k//2, k - k//2)
+        
+        
