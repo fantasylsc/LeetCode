58191692c5329888910ec9b220393137c095be7f
fantasylsc <32818137+fantasylsc@users.noreply.github.com>
2020-02-23 13:00:27 -0700

    Update 0010_Regular_Expression_Matching.py

--- Algorithm/Python/25/0010_Regular_Expression_Matching.py
+++ Algorithm/Python/25/0010_Regular_Expression_Matching.py
@@ -67,5 +67,23 @@ class Solution:
             
         return self.isMatch(s, p[2:])
 
+# Recursion
+
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        if not p: # p is empty
+            return not s
+
+        first_match = bool(s) and p[0] in {s[0], '.'} # whether first character matches
+
+        if len(p) >= 2 and p[1] == '*': # p[1] == '*'
+            return (self.isMatch(s, p[2:]) or # repeat 0 time means delete
+                    first_match and self.isMatch(s[1:], p)) # first_match, repeat p[0]
+        else: # p[1] != '*', match the rest string
+            return first_match and self.isMatch(s[1:], p[1:])   
+    
+    
+    
+    
 
 
