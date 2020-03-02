e37b9848863cf1fb8a78072b8f8d9021a05632cd
fantasylsc <32818137+fantasylsc@users.noreply.github.com>
2020-03-01 23:12:07 -0700

    Update 0012_Integer_to_Roman.py

--- Algorithm/Python/25/0012_Integer_to_Roman.py
+++ Algorithm/Python/25/0012_Integer_to_Roman.py
@@ -56,6 +56,21 @@ class Solution:
             
         return res
 
-
+    
+# Another approach
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        nums = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+        dic = {1000: 'M', 900: 'CM', 500: 'D', 400: 'CD', 100: 'C', 90: 'XC',
+              50: 'L', 40: 'XL', 10: 'X', 9: 'IX', 5: 'V', 4: 'IV', 1: 'I'
+              }
+        res = ''
+        
+        for n in nums:
+            while num >= n:
+                num -= n
+                res += dic[n]
+                
+        return res
 
 
