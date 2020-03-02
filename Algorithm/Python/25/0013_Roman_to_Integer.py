8284cc0d6147b2ed3f033e110c0a8d12c262f539
fantasylsc <32818137+fantasylsc@users.noreply.github.com>
2020-03-01 23:26:48 -0700

    Update 0013_Roman_to_Integer.py

--- Algorithm/Python/25/0013_Roman_to_Integer.py
+++ Algorithm/Python/25/0013_Roman_to_Integer.py
@@ -61,6 +61,21 @@ class Solution:
         
         return res
 
-
+# same using for loop
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        res = 0
+        
+        for i in range(len(s)):
+            if i == len(s) - 1:
+                res += dic[s[i]]
+            else:
+                if dic[s[i]] >= dic[s[i + 1]]:
+                    res += dic[s[i]]
+                else:
+                    res -= dic[s[i]]
+        
+        return res
 
 
