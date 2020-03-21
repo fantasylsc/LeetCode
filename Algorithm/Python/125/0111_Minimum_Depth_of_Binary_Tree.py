a4d5267f31af62522810ec68a836d3ab05488e8a
fantasylsc <32818137+fantasylsc@users.noreply.github.com>
2020-03-20 20:27:55 -0600

    Update 0111_Minimum_Depth_of_Binary_Tree.py

--- Algorithm/Python/125/0111_Minimum_Depth_of_Binary_Tree.py
+++ Algorithm/Python/125/0111_Minimum_Depth_of_Binary_Tree.py
@@ -27,6 +27,10 @@ return its minimum depth = 2.
 #         self.left = None
 #         self.right = None
 
+# The rule of checking min_depth:
+# when comes to a leaf node, update the min_depth
+# BFS will stop at a level that has a leaf node
+# DFS will traverse through all the nodes in the tree
 
 # BFS
 
@@ -50,4 +54,40 @@ class Solution:
                     q.append(node.right)
                 if node.left:
                     q.append(node.left)
+
+# DFS recursion
+
+# class Solution:
+#     def minDepth(self, root: TreeNode) -> int:
+#         if not root:
+#             return 0
+#         if not root.left and not root.right:
+#             return 1
+        
+#         min_depth = float('inf')
+#         if root.left:
+#             min_depth = min(min_depth, self.minDepth(root.left))
+#         if root.right:
+#             min_depth = min(min_depth, self.minDepth(root.right))
+#         return min_depth + 1
+
+# DFS iterative
+
+# class Solution:
+#     def minDepth(self, root: TreeNode) -> int:
+#         if not root:
+#             return 0
+#         min_depth = float('inf')
+#         stack = []
+#         stack.append((root, 1))
+        
+#         while stack:
+#             node, depth = stack.pop()
+#             if not node.left and not node.right:
+#                 min_depth = min(min_depth, depth)
+#             if node.left:
+#                 stack.append((node.left, depth + 1))
+#             if node.right:
+#                 stack.append((node.right, depth + 1))
+#         return min_depth
         
