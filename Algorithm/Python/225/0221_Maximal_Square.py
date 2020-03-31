87d4f6d254cb9b6349d79d713719908665a1bbf6
fantasylsc <32818137+fantasylsc@users.noreply.github.com>
2020-03-30 18:00:34 -0600

    Add files via upload

--- Algorithm/Python/225/0221_Maximal_Square.py
+++ Algorithm/Python/225/0221_Maximal_Square.py
@@ -1,109 +1,85 @@
 '''
 
-A city's skyline is the outer contour of the silhouette formed by all the buildings in that city when viewed from a distance. Now suppose you are given the locations and height of all the buildings as shown on a cityscape photo (Figure A), write a program to output the skyline formed by these buildings collectively
+Given a 2D binary matrix filled with 0's and 1's, find the largest square containing only 1's and return its area.
 
-The geometric information of each building is represented by a triplet of integers [Li, Ri, Hi], where Li and Ri are the x coordinates of the left and right edge of the ith building, respectively, and Hi is its height. It is guaranteed that 0 ≤ Li, Ri ≤ INT_MAX, 0 < Hi ≤ INT_MAX, and Ri - Li > 0. You may assume all buildings are perfect rectangles grounded on an absolutely flat surface at height 0.
+Example:
 
-For instance, the dimensions of all buildings in Figure A are recorded as: [ [2 9 10], [3 7 15], [5 12 12], [15 20 10], [19 24 8] ] .
+Input: 
 
-The output is a list of "key points" (red dots in Figure B) in the format of [ [x1,y1], [x2, y2], [x3, y3], ... ] that uniquely defines a skyline. A key point is the left endpoint of a horizontal line segment. Note that the last key point, where the rightmost building ends, is merely used to mark the termination of the skyline, and always has zero height. Also, the ground in between any two adjacent buildings should be considered part of the skyline contour.
+1 0 1 0 0
+1 0 1 1 1
+1 1 1 1 1
+1 0 0 1 0
 
-For instance, the skyline in Figure B should be represented as:[ [2 10], [3 15], [7 12], [12 0], [15 10], [20 8], [24, 0] ].
+Output: 4
 
-Notes:
-
-The number of buildings in any input list is guaranteed to be in the range [0, 10000].
-The input list is already sorted in ascending order by the left x position Li.
-The output list must be sorted by the x position.
-There must be no consecutive horizontal lines of equal height in the output skyline. For instance, [...[2 3], [4 5], [7 5], [11 5], [12 7]...] is not acceptable; the three lines of height 5 should be merged into one in the final output as such: [...[2 3], [4 5], [12 7], ...]
 
 '''
 
 
 
-# Heap
-
-# Divide and Conquer
+# DP approach 1
+# dp[i][j] = min(dp[i - 1][j], dp[i - 1][j - 1], dp[i][j - 1]) + 1
+# dp[i][j] represents the side length of the maximum square whose bottom right corner is the cell with index (i,j) in the original matrix.
 
 class Solution:
-    def getSkyline(self, buildings: List[List[int]]) -> List[List[int]]:
-        """
-        Divide-and-conquer algorithm to solve skyline problem,
-        which is similar with the merge sort algorithm.
-        """
-        n = len(buildings)
-        # The base cases
-        if n == 0:
-            return []
-        if n == 1:
-            x_start, x_end, y = buildings[0]
-            return [[x_start, y], [x_end, 0]] 
-         
-        # If there is more than one building, 
-        # recursively divide the input into two subproblems.
-        left_skyline = self.getSkyline(buildings[: n // 2])
-        right_skyline = self.getSkyline(buildings[n // 2 :])
+    def maximalSquare(self, matrix: List[List[str]]) -> int:
+        rows = len(matrix)
+        if rows:
+            cols = len(matrix[0])
+        else:
+            cols = 0
         
-        # Merge the results of subproblem together.
-        return self.merge_skylines(left_skyline, right_skyline)
-    
-    def merge_skylines(self, left, right):
-        """
-        Merge two skylines together.
-        """
-        def update_output(x, y):
-            """
-            Update the final output with the new element.
-            """
-            # if skyline change is not vertical - 
-            # add the new point
-            if not output or output[-1][0] != x:
-                output.append([x, y])
-            # if skyline change is vertical - 
-            # update the last point
-            else:
-                output[-1][1] = y
+        dp = [[0]* cols for _ in range(rows)]
+        maxsqlen = 0
         
-        def append_skyline(p, lst, n, y, curr_y):
-            """
-            Append the rest of the skyline elements with indice (p, n)
-            to the final output.
-            """
-            while p < n: 
-                x, y = lst[p]
-                p += 1
-                if curr_y != y:
-                    update_output(x, y)
-                    curr_y = y
-                
-        n_l, n_r = len(left), len(right)
-        p_l = p_r = 0
-        curr_y  = left_y = right_y = 0
-        output = []
+        for i in range(rows):
+            dp[i][0] = int(matrix[i][0])
+            if dp[i][0] == 1:
+                maxsqlen = 1
             
-        # while we're in the region where both skylines are present
-        while p_l < n_l and p_r < n_r:
-            point_l, point_r = left[p_l], right[p_r]
-            # pick up the smallest x
-            if point_l[0] < point_r[0]: 
-                x, left_y = point_l
-                p_l += 1
-            else: 
-                x, right_y = point_r 
-                p_r += 1
-            # max height (i.e. y) between both skylines
-            max_y = max(left_y, right_y)
-            # if there is a skyline change
-            if curr_y != max_y:
-                update_output(x, max_y)
-                curr_y = max_y
-
-        # there is only left skyline
-        append_skyline(p_l, left, n_l, left_y, curr_y)
-
-        # there is only right skyline
-        append_skyline(p_r, right, n_r, right_y, curr_y)
-                
-        return output        
+        for j in range(cols):
+            dp[0][j] = int(matrix[0][j])
+            if dp[0][j] == 1:
+                maxsqlen = 1
         
+        for i in range(1, rows):
+            for j in range(1, cols):
+                if matrix[i][j] == '1':
+                    dp[i][j] = min(dp[i - 1][j], dp[i - 1][j - 1], dp[i][j - 1]) + 1
+                    if dp[i][j] > maxsqlen:
+                        maxsqlen = dp[i][j]
+        return maxsqlen * maxsqlen
+
+        
+# Brute force
+
+# class Solution:
+#     def maximalSquare(self, matrix: List[List[str]]) -> int:
+#         rows = len(matrix)
+#         if rows:
+#             cols = len(matrix[0])
+#         else:
+#             cols = 0
+#         max_sqlen = 0
+        
+#         for i in range(rows):
+#             for j in range(cols):
+#                 if matrix[i][j] == '1':
+#                     sqlen = 1
+#                     flag = True
+#                     while sqlen + i < rows and sqlen + j < cols and flag:
+#                         for k in range(j, j + sqlen + 1):
+#                             if matrix[i + sqlen][k] == '0':
+#                                 flag = False
+#                                 break
+#                         for k in range(i, i + sqlen + 1):
+#                             if matrix[k][j + sqlen] == '0':
+#                                 flag = False
+#                                 break
+#                         if flag:
+#                             sqlen += 1
+#                     if max_sqlen < sqlen:
+#                         max_sqlen = sqlen
+#         return max_sqlen * max_sqlen       
         
