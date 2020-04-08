4339e905d6e584b3e2f6b2c2b7efe995e2026211
fantasylsc <32818137+fantasylsc@users.noreply.github.com>
2020-04-07 23:53:48 -0600

    Update Backpack.py

--- Approach/Classification/Backpack.py
+++ Approach/Classification/Backpack.py
@@ -23,34 +23,73 @@ You can not divide any item into small pieces.
 
 '''
 
+# Approach 1
+
+import unittest
+
 class Solution:
-    """
-    @param m: An integer m denotes the size of a backpack
-    @param A: Given n items with size A[i]
-    @return: The maximum size
-    
-    state: dp[i][j] whether backpack size of j can be exactly filled by first i items 
-    function: dp[i][j] = dp[i - 1][j] or dp[i - 1][j - A[i - 1]]
-    initialize: dp[0][0] = True, dp[0][:] = False
-    """
     def backPack(self, m, A):
         # write your code here
         n = len(A)
         
-        dp = [[False]*(m + 1) for _ in range(n + 1)]
-        dp[0][0] = True
+        dp = [[0]*(m + 1) for _ in range(n + 1)]
         
         for i in range(1, n + 1):
             for j in range(1, m + 1):
-                if j == A[i - 1]: # ith item size = size j
-                    dp[i][j] = True
-                elif j > A[i - 1]: # item size < size j
-                    # not put ith item in backpack or put in backpack
-                    dp[i][j] = dp[i - 1][j] or dp[i - 1][j - A[i - 1]]
-                else: # item size > size j, can't put in
+                if A[i - 1] <= j:
+                    dp[i][j] = max(dp[i - 1][j], A[i - 1] + dp[i - 1][j - A[i - 1]])
+                else:
                     dp[i][j] = dp[i - 1][j]
-        
-        for j in range(m, -1, -1):
-            if dp[n][j] == True:
-                return j
+        return dp[-1][-1]
 
+# Approach 2
+
+# class Solution:
+#     """
+#     @param m: An integer m denotes the size of a backpack
+#     @param A: Given n items with size A[i]
+#     @return: The maximum size
+    
+#     state: dp[i][j] whether backpack size of j can be exactly filled by first i items 
+#     function: dp[i][j] = dp[i - 1][j] or dp[i - 1][j - A[i - 1]]
+#     initialize: dp[0][0] = True, dp[0][:] = False
+#     """
+#     def backPack(self, m, A):
+#         # write your code here
+#         n = len(A)
+        
+#         dp = [[False]*(m + 1) for _ in range(n + 1)]
+#         dp[0][0] = True
+        
+#         for i in range(1, n + 1):
+#             for j in range(1, m + 1):
+#                 if j == A[i - 1]: # ith item size = size j
+#                     dp[i][j] = True
+#                 elif j > A[i - 1]: # item size < size j
+#                     # not put ith item in backpack or put in backpack
+#                     dp[i][j] = dp[i - 1][j] or dp[i - 1][j - A[i - 1]]
+#                 else: # item size > size j, can't put in
+#                     dp[i][j] = dp[i - 1][j]
+        
+#         for j in range(m, -1, -1):
+#             if dp[n][j] == True:
+#                 return j
+
+class Test(unittest.TestCase):
+    def test1(self):
+        A = [3,4,8,5]
+        m = 10
+        expected = 9
+        sol = Solution()
+        actual = sol.backPack(m, A)
+        self.assertEqual(expected, actual)
+
+    def test2(self):
+        A = [2,3,5,7]
+        m = 12
+        expected = 12
+        sol = Solution()
+        actual = sol.backPack(m, A)
+        self.assertEqual(expected, actual)
+
+unittest.main(verbosity = 2)
