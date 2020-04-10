015fdcf4ad4382181650593b959b3e57a48a3412
fantasylsc <32818137+fantasylsc@users.noreply.github.com>
2020-04-10 13:55:59 -0600

    0022_Generate_Parentheses

--- Algorithm/Python/25/0022_Generate_Parentheses.py
+++ Algorithm/Python/25/0022_Generate_Parentheses.py
@@ -1,52 +1,38 @@
 '''
 
-Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
+ Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
 
-Example:
+For example, given n = 3, a solution set is:
 
-Input: 1->2->4, 1->3->4
-Output: 1->1->2->3->4->4
+[
+  "((()))",
+  "(()())",
+  "(())()",
+  "()(())",
+  "()()()"
+]
+
 
 '''
 
 
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+# backtrack
+# think about when to add '(' and when to add ')'
+# we can always add '(' when number of '(' is less than n
+# we can add ')' when the number of ')' is less than '('
 
 class Solution:
-    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
-        dummy = ListNode(0)
-        head = dummy
+    def generateParenthesis(self, n: int) -> List[str]:
+        self.res = []
+        self.backtrack(n, '', 0, 0)
+        return self.res
         
-        while l1 and l2:
-            if l1.val <= l2.val:
-                head.next = l1
-                head = head.next
-                l1 = l1.next
-            else:
-                head.next = l2
-                head = head.next
-                l2 = l2.next
-        
-        if l1:
-            head.next = l1
-            head = head.next
-            l1 = l1.next
-        if l2:
-            head.next = l2
-            head = head.next
-            l2 = l2.next  
-        
-        return dummy.next
-
-
-
+    def backtrack(self, n, current, left, right):
+        if len(current) == 2 * n:
+            self.res.append(current)
+            return
+        if left < n:
+            self.backtrack(n, current + '(', left + 1, right)
+        if right < left:
+            self.backtrack(n, current + ')', left, right + 1)
+         
