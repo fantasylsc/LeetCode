cdf239d4cfb9a3fa1e5a604f58904be958550a24
fantasylsc <32818137+fantasylsc@users.noreply.github.com>
2020-04-11 21:07:37 -0600

    Update 0973_K_Closest_Points_to_Origin.py

--- Algorithm/Python/975/0973_K_Closest_Points_to_Origin.py
+++ Algorithm/Python/975/0973_K_Closest_Points_to_Origin.py
@@ -6,9 +6,7 @@ We have a list of points on the plane.  Find the K closest points to the origin 
 (Here, the distance between two points on a plane is the Euclidean distance.)
 
 You may return the answer in any order.  The answer is guaranteed to be unique (except for the order that it is in.)
-
  
-
 Example 1:
 
 Input: points = [[1,3],[-2,2]], K = 1
@@ -25,18 +23,13 @@ Input: points = [[3,3],[5,-1],[-2,4]], K = 2
 Output: [[3,3],[-2,4]]
 (The answer [[-2,4],[3,3]] would also be accepted.)
 
- 
-
 Note:
 
     1 <= K <= points.length <= 10000
     -10000 < points[i][0] < 10000
     -10000 < points[i][1] < 10000
 
-
-
 '''
-
 
 # approach heap
 # Time O(N*log(M)), where N is points and M is min_heap. 
