264f68dcb8643c19c9eda893866bc9274dd3c5a2
fantasylsc <32818137+fantasylsc@users.noreply.github.com>
2020-04-20 22:27:04 -0600

    Update MIn_Days_to_Bloom.py

--- Approach/Interview/MIn_Days_to_Bloom.py
+++ Approach/Interview/MIn_Days_to_Bloom.py
@@ -1,6 +1,8 @@
 '''
 
-Given an array of roses. roses[i] means rose i will bloom on day roses[i]. Also given an int k, which is the minimum number of adjacent bloom roses required for a bouquet, and an int n, which is the number of bouquets we need. Return the earliest day that we can get n bouquets of roses.
+Given an array of roses. roses[i] means rose i will bloom on day roses[i]. Also given an int k, which is the minimum number of 
+adjacent bloom roses required for a bouquet, and an int n, which is the number of bouquets we need. Return the earliest day 
+that we can get n bouquets of roses.
 
 Example:
 Input: roses = [1, 2, 4, 9, 3, 4, 1], k = 2, n = 2
