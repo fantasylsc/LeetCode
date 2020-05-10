9dd0541c9b4b418c9b82610d082d18c6b32b090f
fantasylsc <32818137+fantasylsc@users.noreply.github.com>
2020-05-09 18:05:48 -0600

    Update 0088_Merge_Sorted_Array.py

--- Algorithm/Python/100/0088_Merge_Sorted_Array.py
+++ Algorithm/Python/100/0088_Merge_Sorted_Array.py
@@ -16,6 +16,30 @@ Output: [1,2,2,3,5,6]
 
 '''
 
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        k = m + n - 1
+        m -= 1
+        n -= 1
+        
+        while m > -1 and n > -1:
+            if nums1[m] >= nums2[n]:
+                nums1[k] = nums1[m]
+                k -= 1
+                m -= 1
+            else:
+                nums1[k] = nums2[n]
+                k -= 1
+                n -= 1
+        
+        while n > -1:
+            nums1[k] = nums2[n]
+            k -= 1
+            n -= 1
+
 # list slicing: nums[i: j] does not include jth item
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
