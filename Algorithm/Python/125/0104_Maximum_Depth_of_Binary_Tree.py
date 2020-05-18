b8736b6f7b12290198cc33008ac0b7da79a2c7ad
fantasylsc <32818137+fantasylsc@users.noreply.github.com>
2020-05-17 23:14:20 -0600

    Update 0104_Maximum_Depth_of_Binary_Tree.py

--- Algorithm/Python/125/0104_Maximum_Depth_of_Binary_Tree.py
+++ Algorithm/Python/125/0104_Maximum_Depth_of_Binary_Tree.py
@@ -39,7 +39,7 @@ class Solution:
         
         
 # Recursion 2
-# Recursive tree traversal approach. Use max to track maxDpeth of the tree.
+# Recursive tree traversal approach. Track and update maxDpeth of the tree.
 
 # class Solution:
 #     def maxDepth(self, root: TreeNode) -> int:
