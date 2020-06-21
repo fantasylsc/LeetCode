dfd923e46e245f286d3e0539f97e8e4ac7e52a4b
fantasylsc <32818137+fantasylsc@users.noreply.github.com>
2020-06-21 14:56:22 -0600

    Create Find_the_Middle_of_a_Linked_List.py

---
+++ Approach/Classification/Find_the_Middle_of_a_Linked_List.py
@@ -0,0 +1,16 @@
+
+
+
+
+#     def findMiddle(self, head):
+#         prev = None
+#         slow = head
+#         fast = head # if use head.next, 1->2, error
+        
+#         while fast and fast.next:
+#             prev = slow
+#             slow = slow.next
+#             fast = fast.next.next
+
+
+
