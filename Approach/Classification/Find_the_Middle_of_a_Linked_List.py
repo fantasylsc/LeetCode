abc8e41cf351122ddc872048aef471f8008a0e3f
fantasylsc <32818137+fantasylsc@users.noreply.github.com>
2020-06-21 14:59:29 -0600

    Update Find_the_Middle_of_a_Linked_List.py

--- Approach/Classification/Find_the_Middle_of_a_Linked_List.py
+++ Approach/Classification/Find_the_Middle_of_a_Linked_List.py
@@ -1,16 +1,28 @@
+# find the prvious middle node
+def findMiddle(self, head):
+    prev = None
+    slow = head
+    fast = head.next
+    
+    while fast and fast.next:
+        prev = slow
+        slow = slow.next
+        fast = fast.next.next
 
 
 
 
-#     def findMiddle(self, head):
-#         prev = None
-#         slow = head
-#         fast = head # if use head.next, 1->2, error
-        
-#         while fast and fast.next:
-#             prev = slow
-#             slow = slow.next
-#             fast = fast.next.next
+
+# find the back middle node 
+def findMiddle(self, head):
+    prev = None
+    slow = head
+    fast = head
+    
+    while fast and fast.next:
+        prev = slow
+        slow = slow.next
+        fast = fast.next.next
 
 
 
