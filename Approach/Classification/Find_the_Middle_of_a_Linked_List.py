b2e23bf7a95b20b9d034fd06a1294f78bcca88f3
fantasylsc <32818137+fantasylsc@users.noreply.github.com>
2020-06-21 15:15:26 -0600

    Update Find_the_Middle_of_a_Linked_List.py

--- Approach/Classification/Find_the_Middle_of_a_Linked_List.py
+++ Approach/Classification/Find_the_Middle_of_a_Linked_List.py
@@ -9,21 +9,28 @@ def findMiddle(self, head):
         prev = slow
         slow = slow.next
         fast = fast.next.next
+        
     return slow
 
 
 
 
 # find the back middle node 
+# use this for convert sorted list to BST
 def findMiddle(self, head):
     prev = None
     slow = head
     fast = head 
+    # if use head.next, 1->2->3->4
+    # mid = 2, left = 1, right = 3->4
+    # 3,4, mid = 3 = head(base case condition)
+    # return root(3), wrong
     
     while fast and fast.next:
         prev = slow
         slow = slow.next
         fast = fast.next.next
+        
     return slow
 
 
