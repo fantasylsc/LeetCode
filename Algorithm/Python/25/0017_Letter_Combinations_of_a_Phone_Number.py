bf1cef5d0928a3dbcec9d0a4f57c72169944db52
fantasylsc <32818137+fantasylsc@users.noreply.github.com>
2020-07-05 11:06:08 -0600

    Update 0017_Letter_Combinations_of_a_Phone_Number.py

--- Algorithm/Python/25/0017_Letter_Combinations_of_a_Phone_Number.py
+++ Algorithm/Python/25/0017_Letter_Combinations_of_a_Phone_Number.py
@@ -64,7 +64,7 @@ class Solution:
         dic = {'2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl', '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'}
         
         for i in range(len(digits)):
-            temp = []
+            temp = [] # for each digit create a temp list
             letters = dic[digits[i]]
             for j in range(len(letters)):
                 for s in res:
