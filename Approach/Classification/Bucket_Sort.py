01a15d2ac179132c1efca904fc9d94e06d981603
fantasylsc <32818137+fantasylsc@users.noreply.github.com>
2022-01-27 13:47:51 -0700

    Update Bucket_Sort.py

--- Approach/Classification/Bucket_Sort.py
+++ Approach/Classification/Bucket_Sort.py
@@ -48,7 +48,48 @@ class Solution:
             b[j + 1] = key      
         return b
 
-
+    
+# Bucket sort for numbers
+# having integer part
+def bucketSort(arr, noOfBuckets):
+    max_ele = max(arr)
+    min_ele = min(arr)
+ 
+    # range(for buckets)
+    rnge = (max_ele - min_ele) / noOfBuckets
+ 
+    temp = []
+ 
+    # create empty buckets
+    for i in range(noOfBuckets):
+        temp.append([])
+ 
+    # scatter the array elements
+    # into the correct bucket
+    for i in range(len(arr)):
+        diff = (arr[i] - min_ele) / rnge -
+              int((arr[i] - min_ele) / rnge)
+ 
+        # append the boundary elements to the lower array
+        if(diff == 0 and arr[i] != min_ele):
+            temp[int((arr[i] - min_ele) / rnge) - 1].append(arr[i])
+ 
+        else:
+            temp[int((arr[i] - min_ele) / rnge)].append(arr[i])
+ 
+    # Sort each bucket individually
+    for i in range(len(temp)):
+        if len(temp[i]) != 0:
+            temp[i].sort()
+ 
+    # Gather sorted elements
+    # to the original array
+    k = 0
+    for lst in temp:
+        if lst:
+            for i in lst:
+                arr[k] = i
+                k = k+1
 
 class Test(unittest.TestCase):
     def test(self):
