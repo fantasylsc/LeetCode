3564462e86c828c0e88f7cb25d771e17ed217e6e
fantasylsc <32818137+fantasylsc@users.noreply.github.com>
2022-08-08 14:00:31 -0600

    Update Topological_Sorting.py

--- Approach/Classification/Topological_Sorting.py
+++ Approach/Classification/Topological_Sorting.py
@@ -57,5 +57,117 @@ g.addEdge(3, 1);
   
 g.topologicalSort() 
         
-        
-        
+# Graph
+
+# 1.Detect cycle in graph
+
+# 207. Course Schedule
+# 1.1 DFS
+
+class Solution(object):
+    def canFinish(self, numCourses, prerequisites):
+        """
+        :type numCourses: int
+        :type prerequisites: List[List[int]]
+        :rtype: bool
+        """
+        from collections import defaultdict
+        courseDict = defaultdict(list)
+
+        for relation in prerequisites:
+            nextCourse, prevCourse = relation[0], relation[1]
+            courseDict[prevCourse].append(nextCourse)
+
+        checked = [False] * numCourses
+        path = [False] * numCourses
+
+        for currCourse in range(numCourses):
+            if self.isCyclic(currCourse, courseDict, checked, path):
+                return False
+        return True
+
+
+    def isCyclic(self, currCourse, courseDict, checked, path):
+        """   """
+        # 1). bottom-cases
+        if checked[currCourse]:
+            # this node has been checked, no cycle would be formed with this node.
+            return False
+        if path[currCourse]:
+            # came across a marked node in the path, cyclic !
+            return True
+
+        # 2). postorder DFS on the children nodes
+        # mark the node in the path
+        path[currCourse] = True
+
+        ret = False
+        # postorder DFS, to visit all its children first.
+        for child in courseDict[currCourse]:
+            ret = self.isCyclic(child, courseDict, checked, path)
+            if ret: break
+
+        # 3). after the visits of children, we come back to process the node itself
+        # remove the node from the path
+        path[currCourse] = False
+
+        # Now that we've visited the nodes in the downstream,
+        #   we complete the check of this node.
+        checked[currCourse] = True
+        return ret
+
+# 1.2 Topological sort
+
+class GNode(object):
+    """  data structure represent a vertex in the graph."""
+    def __init__(self):
+        self.inDegrees = 0
+        self.outNodes = []
+
+class Solution(object):
+    def canFinish(self, numCourses, prerequisites):
+        """
+        :type numCourses: int
+        :type prerequisites: List[List[int]]
+        :rtype: bool
+        """
+        from collections import defaultdict, deque
+        # key: index of node; value: GNode
+        graph = defaultdict(GNode)
+
+        totalDeps = 0
+        for relation in prerequisites:
+            nextCourse, prevCourse = relation[0], relation[1]
+            graph[prevCourse].outNodes.append(nextCourse)
+            graph[nextCourse].inDegrees += 1
+            totalDeps += 1
+
+        # we start from courses that have no prerequisites.
+        # we could use either set, stack or queue to keep track of courses with no dependence.
+        nodepCourses = deque()
+        for index, node in graph.items():
+            if node.inDegrees == 0:
+                nodepCourses.append(index)
+
+        removedEdges = 0
+        while nodepCourses:
+            # pop out course without dependency
+            course = nodepCourses.pop()
+
+            # remove its outgoing edges one by one
+            for nextCourse in graph[course].outNodes:
+                graph[nextCourse].inDegrees -= 1
+                removedEdges += 1
+                # while removing edges, we might discover new courses with prerequisites removed, i.e. new courses without prerequisites.
+                if graph[nextCourse].inDegrees == 0:
+                    nodepCourses.append(nextCourse)
+
+        if removedEdges == totalDeps:
+            return True
+        else:
+            # if there are still some edges left, then there exist some cycles
+            # Due to the dead-lock (dependencies), we cannot remove the cyclic edges
+            return False
+
+
+
